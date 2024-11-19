443391589ae74d89a1178bacad9aab1eca97e1d5
techzohaib <malikwasi77778686@gmail.com>
2024-11-19 23:02:05 +0500

    initial commit

--- main.java
+++ main.java
@@ -17,5 +17,27 @@ public class main{
             }
             System.out.println();
         }
+    
+    int rmin=0;
+    int cmin=0;
+    int rmax=n-1;
+    int cmax=m-1;
+    int count=0;
+    while(count<n*m){
+        for(int row=rmin;row<=rmax&& count <n*m;row++){
+            System.out.println(a1[row][cmin]);
+            count++;
+        }
+        cmin++;
+        for(int col=cmin;col<=cmax&&count<n*m;col++){
+            System.out.println(a1[rmax][col]);
+            count++;
+        }
+        rmax--;
+        for(int row=rmax;row>=rmin && count <n*m;row--){
+            System.out.println(a1[row][cmax]);
+            count++;
+        }
+    }
     }
 }
